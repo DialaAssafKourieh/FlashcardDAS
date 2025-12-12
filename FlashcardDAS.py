456403adc206775f87ef5d5501ccbf23ef6dd760
Diala Assaf Kourieh <diala.assafkourieh@students.fhnw.ch>
2025-12-12 16:08:55 +0100

    change comments

--- FlashcardDAS.py
+++ FlashcardDAS.py
@@ -1,82 +1,82 @@
 '''🎴🎴🎴 Flashcard Program:
-The user will receive a random set of seven (7)quiz questions using the Random_question module
-from three categories: Biology, Geography, and History,which they can choose between.
-After completing the quiz, the user will be asked whether the  want to move to another category
-or end the quiz. Each time, the program will display the user’s score and the number of 
+The user will receive a random set of seven (7) quiz questions using the Random_question module
+from three categories: Biology, Geography, and History which they can choose from.
+After completing the quiz, the user will be asked whether they want to move to another category
+or end the quiz. Each time the program will display the user’s score and the number of 
 questions answered.'''
 
-#🛬Import Random_question module that choose the question from each category randomly
+# 🛬Import Random_question module which chooses the questions randomly from each category
 import Random_question as Rq
-#🤷‍♀️Define new type of error called InvalidCategoryErro
+# 🤷‍♀️Defines new type of error called InvalidCategoryError
 class InvalidCategoryError(Exception):
-#Define an empty block,Helps keep the code syntactically correct while you’re still developing.    
+# Defines an empty block, helps keep the code syntactically correct while you’re still developing.
     pass
 
 # define main function to ask the user and choose the category
 def main():
-    #assign numbers to each category
+    # assign numbers to each category
     Biology=1
     Geography=2
     History=3
     End=4
         
-    # ask the user if he want to start quizz if not the program will be ended
-    #the program will be repeatet every time the answer is y
+    # ask the user if he want to start the quiz, if not the program will be terminated
+    # the program will be repeated every time the answer is y
     while True:
-        #🧙ask the user if he want to play
-        ques=input('Are you ready to play Flash Cards quizz 🎴 🎴 ? press 👍 Y or 👎 N :')
+        # 🧙ask the user if he wants to play
+        ques=input('Are you ready to play the Flashcards quiz ? Enter 👍 Y or 👎 N :')
         
-        #we check if ques = y ,using lower function to convert the letter to the lower case
-        #it repeat asking question if ques was not in (y,n)
+        # check if ques = y using lower function to make it lowercase
+        # ask for input again if ques was not in (y,n)
         while  ques.lower() not in ['y','n']:
-               print ('Invalid input.Enter only Y 👍 or N 👎.')
-               ques=input('Are you ready to play Flash Cards quizz? press 🎴🎴 Y or N :')
+               print ('Invalid input. Enter only Y 👍 or N 👎')
+               ques=input('Are you ready to play the Flashcards quiz? Enter 👍 Y or 👎 N :')
        
         if ques.lower() =='n' :     
            print ('Enjoy Your Life 🍺.') 
-           break #Exit program
-       #in case the answer was not 'n'
-        print ('choose a categories or exit the game.')
+           break # Exit program
+       # In case the answer was not 'n'
+        print ('Choose a category or exit the game.')
         print ('1• 🧠  Biology')
-        print ('2• 🏝️  Geography') 
+        print ('2• 🏝️  Geography')
         print ('3• 📗  History ')
-        print ('4• 🙋  To Exit tap 4')
+        print ('4• 🙋  To Exit enter 4')
         
-        #create a loop while the answer is y
+        # create a loop that runs while the answer is y
         
         while True: 
               try:   
-                    cat=int(input('🔢Choose the category (1-4) :')) 
+                    cat=int(input('🔢Choose a category (1-4) :')) 
                     if cat not in (1, 2, 3, 4) :
-                        #raise an error in case the category is a number but not in the range 1--4
-                       raise InvalidCategoryError("🔢Category must be between 1 and 4! ")
-                       #print("🔢Category must be between 1 and 4! ")
+                        # raise an error in case the category is a number but not in the range 1-4
+                       raise InvalidCategoryError("🔢Category must be between 1 and 4.")
+                       #print("🔢Category must be between 1 and 4!")
                     break 
               except ValueError:
-                        print('Invalid input, you must entre a number from 1 to 4 🔢.')
+                        print('Invalid input, you must enter a number from 1 to 4 🔢.')
               except InvalidCategoryError as e:
                             print(e)
-        #we will handle the case cat is in (1,2,3,4)
+        #  handles the case when cat is 1, 2, 3 or 4
         if  cat==1:
                 sheetname = 'Biology'
-                print('you have 7 question in biology 🧠')
-                #🧠 call random_question for the sheetname Biology
+                print('You have 7 questions in Biology 🧠')
+                #🧠 call random_question for Biology
                 Rq.rand_ques(sheetname)
                 
         elif cat==2:
                 sheetname = 'Geography'
-                print('you have 7 question in Geography 🏝️')
-                #🏝️call random_question for the sheetname Geography
+                print('You have 7 questions in Geography 🏝️')
+                #🏝️call random_question for Geography
                 Rq.rand_ques(sheetname)
                 
         elif cat==3:
                 sheetname = 'History1'
-                print('you have 7 question in Histoy📗 ')
-                #📗call random_question for the sheetname History
+                print('You have 7 questions in History 📗')
+                #📗call random_question for History
                 Rq.rand_ques(sheetname)
                 
         elif cat==4:
-                #end the quizz
+                # end the quiz
                 print('Enjoy your life 🍺.')
                 exit(0)
                       
--- Random_question.py
+++ Random_question.py
@@ -1,40 +1,39 @@
-'''The modoul Random_question is a module that choose the 
-   random question from excel file FlaschcardDAS.xlsx. 
-   we call it from the main program  Rq.rand_ques(sheetname) where sheetname 
-   in excel file that contains the category'''
+'''The module Random_question chooses the 
+   random questions from the excel file FlaschcardDAS.xlsx. 
+   We call it from the main program Rq.rand_ques(sheetname) where the sheetname
+   refers to the chosen category '''
 
-#panda helps to handle data
+# panda is a python library that helps us to read the excel file
 import pandas as pd
 import random
 import os
 file_path=('FlashcardsDAS.xlsx')
-#define the function that is called from the main program for the argument Biology
+# define the function that is called in the main program for the argument Biology
 def rand_ques(sheet_to_read='Biology'):
    try:
        df = pd.read_excel('FlashcardsDAS.xlsx', sheet_name=sheet_to_read)
 
-        #sheet_name=['Biology','Geography','History1'])
-        # Select 7 random rows without repetition using sample function repace =falce not to repeat
+        # sheet_name=['Biology','Geography','History1'])
+        # Select 7 random rows without repetition using the sample function.
     
        random_rows = df.sample(n=7, replace=False)
        score = 0
        count = 0
 
-        #iterate over the rows of a DataFrame as (index, Series) pairs
+        # iterate over the rows of the excel file as (index, Series) pairs so that the row for questions corresonds with the row for answers
 
        for index, row in random_rows.iterrows():
             question = row["Questions"]
             correct_answer = row["Answers"] 
 
-            #print the question
+            # print the question
             print(f"\n Question {count+1}: {question}")
 
-            #removes any extra whitespace at the start and end of the string.
+            # removes any extra white space at the start and end of the string.
             answer = input("Enter your answer: ").strip()
 
             # Case-insensitive comparison
             if answer.lower() == str(correct_answer).lower():
-                # get emoji by precing windows plus .
                     print("✅ Correct answer.")
                     score += 1
             else:
@@ -45,7 +44,7 @@ def rand_ques(sheet_to_read='Biology'):
 
             print(f"\nQuiz completed! Your final score is {score}/7.")
    except FileNotFoundError:
-       print('Error The File Flashcards.xlsx is not fount.Make sure the file e it exist')        
+       print('Error The File Flashcards.xlsx is not found. Make sure the file exists.')        
        return
 # Run the quiz
 if __name__ == '__main__':
